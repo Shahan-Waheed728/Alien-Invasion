321cefc4f19906f3669071bb06e62c38122b0edd
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-08-10 09:15:40 +0500

    Add bullet system and sprite group management

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship 
 import game_functions as gf 
+from pygame.sprite import Group
 def run_game():
     pygame.init()
     """Initialize pygame,settings and screen objects."""
@@ -12,11 +13,14 @@ def run_game():
     # bg_color = ((24,24,43))
     # Make a ship 
     ship = Ship(ai_settings,screen)
+    # Make a group the store bullets
+    bullets = Group()
     # Start the loop for game 
     while True:
-        gf.check_events(ship)  
+        gf.check_events(ai_settings,screen,ship,bullets)  
         ship.update()      
-        gf.update_screen(ai_settings,screen,ship)   
+        bullets.update()
+        gf.update_screen(ai_settings,screen,ship,bullets)   
 run_game()
 
         
