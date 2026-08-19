e8e75e25bcf49fbf8ccd635dc51b1f02dd7696b3
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-08-19 09:02:06 +0500

    Adding Game Over functionality

--- game_functions.py
+++ game_functions.py
@@ -107,16 +107,28 @@ def change_fleet_direction(ai_settings,aliens):
     ai_settings.fleet_direction *= -1
 def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
     """Respond to ship being hit by alien."""
-    #Decrement ship_left
-    stats.ship_left -= 1
-    # Empty the list of aliens and bullets 
-    aliens.empty()
-    bullets.empty()
-    # Create a new fleet and center the ship 
-    create_fleet(ai_settings,screen,ship,aliens)
-    #Pause
-    sleep(0.5)
+    if stats.ship_left > 0:
+        #Decrement ship_left
+        stats.ship_left -= 1
+        # Empty the list of aliens and bullets 
+        aliens.empty()
+        bullets.empty()
+        # Create a new fleet and center the ship 
+        create_fleet(ai_settings,screen,ship,aliens)
+        #Pause
+        sleep(0.5)
+    else:
+        stats.game_active = False
     ship.center_ship()
+def check_aliens_bottom(ai_setting,stats,screen,ship,aliens,bullets):
+    """Check if any aliens have reached the bottom of the screen"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as ship got hit 
+            ship_hit(ai_setting,stats,screen,ship,aliens,bullets)
+            break
+
 def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     """Check if the fleet is at an edge then update the position of all aliens in the fleet."""
     check_fleet_edges(ai_settings,aliens)
@@ -124,3 +136,5 @@ def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     # Look for alien ship collisions 
     if pygame.sprite.spritecollideany(ship,aliens):
         ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+    # Look for aliens hitting the bottom 
+    check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
--- settings.py
+++ settings.py
@@ -16,6 +16,6 @@ class Settings():
         self.ship_limit = 3
         # Alien settings
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 3
+        self.fleet_drop_speed = 10
         # fleet direction 1 represents right, -1 represents left 
         self.fleet_direction = 1
